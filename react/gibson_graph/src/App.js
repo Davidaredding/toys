70d6ed51c45b10aed3001392f714990a349c419b
Dave <Davidaredding@Gmail.com>
2018-10-04 15:56:10 -0400

    redux

--- react/gibson_graph/src/App.js
+++ react/gibson_graph/src/App.js
@@ -1,32 +1,55 @@
 import React, { Component } from 'react';
 import addMessage from './Actions/simpleAction';
-import store from './store'
+import {connect} from 'react-redux';
+
+
+//import store from './store'
 
 //import logo from './logo.svg';
 import './App.css';
 
 
+
 //https://redux.js.org/basics/dataflow
 //https://redux.js.org/basics/usagewithreact
 
+const mapStateToProps = state =>{
+  return {
+    MOTD: state.motd
+  }
+}
+
+let interval;
+
+//This injects the 'motdClick' as a prop
+const mapDispatchToProps = dispatch=>{
+  return{
+    motdClick: m=>{
+      if(interval === undefined)
+        interval = setInterval(()=>dispatch(addMessage(Date.now())), 1);
+      else
+        interval = clearInterval(interval);
+    }
+  }
+}
 
 
 class App extends Component {
-  constructor(){
-    super();
-    store.subscribe(()=>this.props.MOTD = store.getState().motd);
-  }
+//   constructor(){
+//     super();
+// //    store.subscribe(()=>console.log(this));
+//   }
   
-  messageAction = ()=>{
-    console.log(store.getState());
-    store.dispatch(addMessage("Hello"));
-    store.dispatch(addMessage(Date.now()));
-  }
+  // messageAction = ()=>{
+  //   console.log(store.getState());
+  //   store.dispatch(addMessage("Hello"));
+  //   store.dispatch(addMessage(Date.now()));
+  // }
   render() {
     return (
      
       <div className="App">
-        <button onClick={this.messageAction}>Test redux action</button>
+        <button onClick={this.props.motdClick}>Test redux action</button>
         <pre>
           {
             JSON.stringify(this.props)
@@ -37,4 +60,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- react/gibson_graph/src/index.js
+++ react/gibson_graph/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import messageApp from './Reducers/reducer'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 import './index.css';
 import App from './App';
 
 
+const store = createStore(
+    messageApp, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+
+
+
+
 
 ReactDOM.render(
-    
-        <App MOTD="" />,
+    <Provider store={store}>
+        <App MOTD="" />
+    </Provider>,
     
     document.getElementById('root')
 );
